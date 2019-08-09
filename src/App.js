bd241b2e90a887236a72970937ab39aecca9fdf0
gergokutu <gergo.kutu@gmail.com>
2019-08-09 12:19:36 +0200

    Clear form after sending and data sent in good format » JSON vs string

--- src/App.js
+++ src/App.js
@@ -13,26 +13,30 @@ class App extends React.Component {
   componentDidMount() {
     // use fat arrow because of the .this
     this.source.onmessage = (event) => {
-
-      const newMessages = [...this.state.messages, event.data]
-
-      this.setState({ messages: newMessages })
+      // we do not want all the messages copied so
+      // removed the newMessages part
+      // const newMessages = [...this.state.messages, event.data]
+      // create JSON from string
+      const messages = JSON.parse(event.data)
+      // clear the input field of the form
+      this.setState({ messages })
     }
   }
 
   onSubmit = async (event) => {
     event.preventDefault()
-    console.log('this.state.message', this.state.message)
+    // console.log('this.state.message', this.state.message)
 
     const response = await request
       .post('http://localhost:5000/message')
-      .send({message: this.state.message})
-      console.log('response test:', response)
+      .send({ message: this.state.message })
+    console.log('response test:', response)
+
+    this.setState({ message: "" })
   }
 
   onChange = (event) => {
     const { value } = event.target
-
     this.setState({ message: value })
   }
 
