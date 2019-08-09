8fe24eff7953d78fad9e6b596f3b8e85e7ab200e
gergokutu <gergo.kutu@gmail.com>
2019-08-09 11:50:06 +0200

    Form added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    message: "",
+    messages: []
+  }
+  // it manages receiving requests from the server..
+  source = new EventSource('http://localhost:5000/stream')
+
+  componentDidMount() {
+    // use fat arrow because of the .this
+    this.source.onmessage = (event) => {
+
+      const newMessages = [...this.state.messages, event.data]
+
+      this.setState({ messages: newMessages })
+    }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    console.log('this.state.message', this.state.message)
+  }
+
+  onChange = (event) => {
+    const { value } = event.target
+
+    this.setState({ message: value })
+  }
+
+  render() {
+    const messages = this.state
+      .messages
+      .map((message, index) => <p key={index}>{message}</p>)
+
+    const form = <form onSubmit={this.onSubmit}>
+      <input type='text' value={this.state.message} onChange={this.onChange} />
+      <button type='submit'>Send</button>
+    </form>
+
+    return <main>
+      {form}
+      {messages}
+    </main>
+  }
 }
 
 export default App;
