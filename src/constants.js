42b55abcc416317a844cbb08505b018fd2ca1333
gergokutu <gergo.kutu@gmail.com>
2019-08-10 15:20:33 +0200

    Heroku link inserted » constant.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:5000'
-// export const url = 'https://nameless-garden-13309.herokuapp.com/'
+// export const url = 'http://localhost:5000'
+export const url = 'https://nameless-garden-13309.herokuapp.com/'
